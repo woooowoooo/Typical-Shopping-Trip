ca234a0e6373ef080c7d268db045abc4767d9244
Ryan <ryan.zhang@digipen.edu>
2020-08-09 18:33:49 -0700

    Scripts and sounds
    Added a bunch of stuff. Most of it is unused.
    Used .flac on Goldberg Aria to squeeze under the 100MB size limit.

---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+	public string Name;
+	private bool Mask;
+	private double maskLevel = 0.6;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		if (1 > maskLevel)
+		{
+			Mask = true;
+		}
+		else
+		{
+			Mask = false;
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
