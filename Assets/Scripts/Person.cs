a9fbcfa3b581422a33fd3d7c4880305da450027c
Ryan <ryan.zhang@digipen.edu>
2020-08-11 13:49:32 -0700

    Made player a person
    Learnt the basics of inheritance in C#.

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class Person : MonoBehaviour
 {
-	public string Name;
-	private bool Mask;
-	private double maskLevel = 0.6;
+	public string fullName = "Zoosmell Pooplord";
+	public int speed = 5;
+	public bool mask = false;
 	
     // Start is called before the first frame update
     void Start()
     {
 		if (1 > maskLevel)
 		{
-			Mask = true;
+			mask = true;
 		}
 		else
 		{
-			Mask = false;
+			mask = false;
 		}
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Person
 {
+	private Vector2 movement;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+		mask = false;
     }
-
+	
     // Update is called once per frame
     void Update()
     {
-		
+		Move();
     }
+	
+	// Does the moving
+	private void Move()
+	{
+		movement = movement.normalized * speed * Time.deltaTime;
+		transform.Translate(movement);
+	}
+	
+	// Movement input handler
+	public void OnMove(InputValue value)
+	{
+		movement = value.Get<Vector2>();
+	}
 }
